275031fb314ceccca22e84ea35deff25535577d3
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-03-31 15:12:52 +0100

    added a state and a click event in order to change the online status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Contact.css'
 import PropTypes from 'prop-types';
 
-function Contact (props) {
-    return (
-        <div className='Contact'>
-            <img className='avatar' src={props.avatar} alt={props.name + 'avatar'}/>
-            <div>
-                 <h4 className='name'>{props.name}</h4>
-                <div className='status'>
-                    <div className={props.online ? 'status-online': 'status-offline'}></div>
-                    <p className='status-text'>{props.online ? 'online': 'offline'}</p>
+class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            online: props.online
+        }
+    }
+    render () {
+        const { name, avatar } = this.props
+        return (
+            <div className='Contact'>
+                <img className='avatar' src={avatar} alt={name + 'avatar'}/>
+                <div>
+                     <h4 className='name'>{name}</h4>
+                    <div className='status' 
+                    onClick={event => {
+                        const newState = !this.state.online;
+                        this.setState({ online: newState });
+                    }} >
+                        <div className={this.state.online ? 'status-online': 'status-offline'}></div>
+                        <p className='status-text'>{this.state.online ? 'online': 'offline'}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-    
+        )
+    }
 }
 
 Contact.propTypes = {
